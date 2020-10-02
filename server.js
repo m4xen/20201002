54ed110c1257aa06a7590cb5f7ce12ca688ce4ae
Max Runesson <Max@maxRunesson.se>
2020-10-02 11:41:34 +0200

    changes in code, and added db

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-//var mysql = require('mysql');
+const express = require('express');
+const mysql = require('mysql');
 
+const app = express();
 
-var app = express();
 app.get('/', function(req, res){
     res.send('Hello from Express');
 })
